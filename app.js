bdcbf90bdacde13b9b779d0166f5bcc221c8d7b2
Brend Van den Eynde <r0883726@student.thomasmore.be>
2023-12-06 17:48:09 +0100

    Add stats view

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const app = express();
 
 // Set up your other middleware and configurations
 
-// Your other routes...
-
 // Route for rendering the updatestats form
 app.get('/updatestats', (req, res) => {
     res.render('updatestats');
+});
+
+// Route for rendering the stats view
+app.get('/', (req, res) => {
+    // For now, let's just send a placeholder message
+    res.send('<h1>Statistics will be displayed here</h1>');
 });
 
 // Your other routes...
