feee965b93cbb3a7b70701be3d920e14111a8e23
Brend Van den Eynde <r0883726@student.thomasmore.be>
2023-12-06 20:12:30 +0100

    Integrate WebSocket for real-time updates
    - Set up Primus for WebSocket communication
    - Add WebSocket event handler for receiving and broadcasting data
    - Update the '/updatestats' route to handle form submissions and update team scores
    - Broadcast updated team scores to all connected clients
    - Implement initial team data broadcast to new WebSocket connections
    - Update comments and improve code readability

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const http = require('http');
 const Primus = require('primus');
 const path = require('path');
+const bodyParser = require('body-parser'); // Import body-parser
 
 const app = express();
 const server = http.createServer(app);
@@ -14,6 +15,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Use body-parser middleware to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Create a Primus instance and integrate it with the server
 const primus = new Primus(server, { transformer: 'websockets' });
@@ -48,6 +52,46 @@ app.get('/updatestats', (_, res) => {
     res.render('updatestats');
 });
 
+// Route for handling the form submission
+// Route for handling the form submission
+app.post('/updatestats', (req, res) => {
+    // Handle the form data here
+    const { team, score } = req.body;
+    console.log(`Received form submission - Team: ${team}, Score: ${score}`);
+
+    // Update the scores in the teams array
+    const foundTeam = teams.find(t => t.name === team);
+    if (foundTeam) {
+        foundTeam.score = parseInt(score, 10); // Parse the score as an integer
+        console.log(`Updated team - ${team}: ${foundTeam.score}`);
+
+        // Broadcast the updated teams to all connected clients
+        primus.write({ event: 'updateStats', teams });
+    } else {
+        console.log(`Team not found - ${team}`);
+    }
+
+    // Log the updated teams array
+    console.log('Updated teams:', teams);
+
+    // Redirect back to the updatestats page or any other page
+    res.redirect('/updatestats');
+});
+
+// Handle WebSocket connections
+primus.on('connection', (spark) => {
+    console.log('Client connected via WebSocket');
+
+    // Send the initial teams data to the newly connected client
+    spark.write({ event: 'updateStats', teams });
+
+    // Handle disconnections
+    spark.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
 // Route for rendering the stats view
 app.get('/stats', (_, res) => {
     res.render('stats', { teams });
@@ -57,11 +101,6 @@ app.get('/stats', (_, res) => {
 app.get('/', (_, res) => {
     res.render('stats', { teams });
 });
-
-
-
-
-// Your other routes...
 
 // Start the server
 const PORT = process.env.PORT || 3000;
